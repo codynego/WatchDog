1a6ca0d5a260f5654653b199e1c893ea50cc0c9e
abednego emonena <emonnenaabednego@gmail.com>
2023-11-09 22:59:02 +0100

    added a server model

--- data_manager/models.py
+++ data_manager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Server(models.Model):
+    hostname = models.CharField(max_length=200)
+    ip_address = models.CharField(max_length=200)
+    memory_capacity = models.IntegerField()
+    cpu_capacity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.hostname} - {self.ip_address}:{self.port}"
